98f6f6f314f7e7b341682b39ef117d1d7ebea26e
Kotori Saki <kitartist1th@gmail.com>
2025-04-13 00:04:04 +0700

    V.0.0.2 (developinng & researching) Add encryption on password before write on the log_file.txt

--- main.cpp
+++ main.cpp
@@ -12,9 +12,14 @@ class standard_object{
     string test_source2;
     string test_key;
 
-    string key = "abcdefg";
+    string default_key = "AZBYCX";
+    string password_key;
+    string key;
+
+    string TEMP;
     vector<string> id;
     vector<string> password;
+    vector<string> en_password;
     vector<string> text;
     vector<string> en_text;
 
@@ -31,16 +36,104 @@ class standard_object{
         }
         return output;
     }
+    void g(){
+        password_key = xorEncryptDecrypt(TEMP,default_key);
+    }
+
     void write_log();
     void read_log();
     void setup_raw_data();
+
     void test_vector();
-    void test_encrytion(){
-        test_vector();
-        write_log();
-    }
+    void test_encryption();
     void test_read_log();
 };
+
+void standard_object::write_log(){
+    string filepassword = "log_file.txt";
+    ofstream outputFile(filepassword);
+    en_text.clear();
+    en_password.clear();
+    for(int i = 0; i < password.size(); i++){en_password.push_back(xorEncryptDecrypt(password.at(i),default_key));}
+    for(int i = 0; i < text.size(); i++){en_text.push_back(xorEncryptDecrypt(text.at(i),default_key));}
+    if (outputFile.is_open()) {
+
+        for(int i = 0; i < id.size(); i++){
+            outputFile << id.at(i) << endl << en_password.at(i) << endl << "#*#*#*#*#*#*#*#*" << endl << en_text.at(i) << endl << "#*#*#*#*#*#*#*#*" << endl; // Write user input to the file
+        }
+
+        outputFile.close(); // Close the file stream
+        cout << "File '" << filepassword << "' written successfully." << endl << "\tPress Enter..."; getchar();
+    } else {
+        cerr << "Error: Could not open file '" << filepassword << "' for writing." << endl;
+    }
+}
+
+void standard_object::setup_raw_data(){//TODO: make set up part at password
+    id.clear(); 
+    en_password.clear();
+    en_text.clear();
+    int top_up = 0;
+    string temp1;
+    short temp2, temp3;
+    for(int i = 0; i < raw_source.size(); i++){
+        if((i - top_up)%4 == 0){id.push_back(raw_source.at(i));}
+        else if((i - top_up)%4 == 1){
+            temp2 = 0;
+            for(int j = 0; ; j++){
+                if(raw_source.at(i + j).compare("#*#*#*#*#*#*#*#*") == 0){temp2 = j; break;}
+            }
+            //cout << temp2 << endl;
+            temp1 = "";
+            for(int j = 0; j < temp2; j++){
+                temp1 += raw_source.at(i + j);
+                if(j != temp2 - 1){temp1 += '\n';}
+            }
+            en_password.push_back(temp1);
+            i += temp2;
+            top_up += temp2;
+        }
+        else if((i - top_up)%4 == 2){
+            temp2 = 0;
+            for(int j = 0; ; j++){
+                if(raw_source.at(i + j).compare("#*#*#*#*#*#*#*#*") == 0){temp2 = j; break;}
+            }
+            //cout << temp2 << endl;
+            temp1 = "";
+            for(int j = 0; j < temp2; j++){
+                temp1 += raw_source.at(i + j);
+                if(j != temp2 - 1){temp1 += '\n';}
+            }
+            en_text.push_back(temp1);
+            i += temp2;
+            top_up += temp2 - 1;//! -1 to let (%4 == 3) Check the point-gate before continue to the next user's data
+        }
+        else if((i - top_up) % 4 == 3 || raw_source.at(i).compare("#*#*#*#*#*#*#*#*") == 0){cout << endl << endl << "Done for user " << (i / 4) + 1 << endl;}
+        ////else{cout << endl << endl << "Error! (setup_raw_data)" << endl; exit(0);}
+    }
+}
+
+void standard_object::read_log(){
+    raw_source.clear();
+    string filepassword = "log_file.txt";
+    ifstream inputFile(filepassword);
+
+    if (inputFile.is_open()){
+        string line;
+        while(getline(inputFile, line)){
+            raw_source.push_back(line);
+        }
+        inputFile.close();
+    } else {
+        cerr << "Error: Could not open file '" << filepassword << "' for reading." << endl;
+    }
+    for(int i = 0; i < raw_source.size(); i++){
+        cout << raw_source.at(i) << endl;
+    }
+    cout << endl << endl << "======================" << endl << endl;
+}
+
+//!====={testing part}==========================================================================
 
 void standard_object::test_vector(){
     string temp1;
@@ -62,128 +155,67 @@ void standard_object::test_vector(){
     cout << endl;
 }
 
-void standard_object::write_log(){
-    string filepassword = "log_file.txt";
-    ofstream outputFile(filepassword);
-    en_text.clear();
-    for(int i = 0; i < text.size(); i++){en_text.push_back(xorEncryptDecrypt(text.at(i),key));}
-    if (outputFile.is_open()) {
-
-        for(int i = 0; i < id.size(); i++){
-            outputFile << id.at(i) << endl << password.at(i) << endl << en_text.at(i) << endl << "#*#*#*#*#*#*#*#*" << endl; // Write user input to the file
-        }
-
-        outputFile.close(); // Close the file stream
-        cout << "File '" << filepassword << "' written successfully." << endl << "\tPress Enter..."; getchar();
-    } else {
-        cerr << "Error: Could not open file '" << filepassword << "' for writing." << endl;
-    }
-}
-
-void standard_object::setup_raw_data(){
-    id.clear(); 
-    password.clear();
-    en_text.clear();
-    int top_up = 0;
-    string temp1;
-    short temp2, temp3;
-    for(int i = 0; i < raw_source.size(); i++){
-        if((i - top_up)%4 == 0){id.push_back(raw_source.at(i));}
-        else if((i - top_up)%4 == 1){password.push_back(raw_source.at(i));}
-        else if((i - top_up)%4 == 2){//TODO: Fix layering array to store all en_text data in one index of vector array
-            temp2 = 0;
-            for(int j = 0; ; j++){
-                if(raw_source.at(i + j).compare("#*#*#*#*#*#*#*#*") == 0){temp2 = j; break;}
-            }
-            cout << temp2 << endl;
-            temp1 = "";
-            for(int j = 0; j < temp2; j++){
-                temp1 += raw_source.at(i + j);
-                if(j != temp2 - 1){temp1 += '\n';}
-            }
-            en_text.push_back(temp1);
-            i += temp2;
-            top_up += temp2 - 1;
-            //TODO: fix evcrytion part that data went wrong in some case and character
-        }
-        else if((i - top_up) % 4 == 3 || raw_source.at(i).compare("#*#*#*#*#*#*#*#*") == 0){}
-        ////else{cout << endl << endl << "Error! (setup_raw_data)" << endl; exit(0);}
-    }
-}
-
-// temp2 = i; loop1:
-
-// if(raw_source.at(temp2 + 1).compare("#*#*#*#*#*#*#*#*") != 0){
-//     activate1 = 1;
-//     temp1 = raw_source.at(temp2) + raw_source.at(temp2 + 1);
-//     top_up++;
-//     if(raw_source.at(temp2 + 2).compare("#*#*#*#*#*#*#*#*") != 0){++i; ++temp2; goto loop1;}
-//      n_text.push_back(temp1);
-// }
-// else if(raw_source.at(temp2 + 1).compare("#*#*#*#*#*#*#*#*") == 0 && activate1 == 1){en_text.push_back(raw_source.at(temp2));}
-// else if(raw_source.at(temp2 + 1).compare("#*#*#*#*#*#*#*#*") == 0){en_text.push_back(raw_source.at(temp2));}
-
-
-void standard_object::read_log(){
-    //raw_source.clear();
-    string filepassword = "log_file.txt";
-    ifstream inputFile(filepassword);
-
-    if (inputFile.is_open()){
-        string line;
-        while(getline(inputFile, line)){
-            raw_source.push_back(line);
-        }
-        inputFile.close();
-    } else {
-        cerr << "Error: Could not open file '" << filepassword << "' for reading." << endl;
-    }
-    for(int i = 0; i < raw_source.size(); i++){
-        cout << raw_source.at(i) << endl;
-    }
-    cout << endl << endl << "======================" << endl << endl;
-    setup_raw_data();
-}
-
 void standard_object::test_read_log(){
     read_log();
+    setup_raw_data();
+    password.clear();
     text.clear();
+    for(int i = 0; i < en_password.size(); i++){
+        password.push_back(xorEncryptDecrypt(en_password.at(i),default_key));
+    }
     for(int i = 0; i < en_text.size(); i++){
-        text.push_back(xorEncryptDecrypt(en_text.at(i),key));
+        text.push_back(xorEncryptDecrypt(en_text.at(i),default_key));
     }
     for(int i = 0; i < id.size(); i++){
-        cout << id.at(i) << endl << password.at(i) << endl << en_text.at(i) << " => " <<text.at(i) << endl << endl;
+        cout << id.at(i) << endl << password.at(i) << endl << en_text.at(i) << " => " << text.at(i) << endl << endl;
     }
     cout << "\tPress Enter..."; getchar();
 }
-    
 
-void starting_command(){
+void standard_object::test_encryption(){
+    test_vector();
+    write_log();
+}
+
+//!=============================================================================================
+
+void command_action(string &call_command){
     standard_object A;
     vector<string> command = {
-        "test_vector","test_encrytion","test_read_log"
+        "test_vector","test_encryption","test_read_log"
     };
-    string temp1;
+    string temp1 = call_command;
     short temp2;
-    while(1){
-        system("clear");//!clear
-        for(int i = 0; i < command.size(); i++){
-            cout << "\t" << i+1 << ". " << command.at(i) << endl;
-        }
-        cout << endl << endl << "\tCommand => "; getline(cin,temp1);
-        //----------------------
-        for(int i = 0; i < command.size(); i++){
-            if(!temp1.compare(command.at(i))){temp2 = i + 1; break;}
-        }
-        switch(temp2){
-            case 1:system("clear");A.test_vector();break;
-            case 2:system("clear");A.test_encrytion();break;
-            case 3:system("clear");A.test_read_log();break;
-        }
+    system("clear");//!clear
+    // for(int i = 0; i < command.size(); i++){
+    //     cout << "\t" << i+1 << ". " << command.at(i) << endl;
+    // }
+    //----------------------
+    for(int i = 0; i < command.size(); i++){
+        if(!temp1.compare(command.at(i))){temp2 = i + 1; break;}
+    }
+    switch(temp2){
+        case 1:system("clear");A.test_vector();break;
+        case 2:system("clear");A.test_encryption();break;
+        case 3:system("clear");A.test_read_log();break;
     }
 }
 
+//?====={call fuctions}=====
+void starting();
+//?=========================
+
 int main(){
-    starting_command();
+    standard_object A;
+    starting();
     return 0;
+}
+
+void starting(){
+    system("clear");
+    string choice1;
+    while(1){
+        cout << endl << endl << "\tCommand => "; getline(cin,choice1);
+        command_action(choice1);
+    }
 }
